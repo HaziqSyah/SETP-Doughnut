57bd485cd5b29daa635384b35c01cb5b89fd6648
HaziqSyah <111070025+HaziqSyah@users.noreply.github.com>
2022-08-11 22:48:20 +0800

    Delete partials directory

--- partials/footer.php
+++
@@ -1,8 +0,0 @@
-           <!-- Footer Section Starts -->
-           <div class="footer">
-                <div class="wrapper">
-                <p class="text-center">2022 All rights reserved, Cupcake.Developed By <a href="#"> SETP Group 8 </a></p>
-                </div> 
-          </div>
-     </body>
-</html>
