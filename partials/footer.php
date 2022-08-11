79e6d20f2f1da3e5c099e3eca757ba171e11da8c
HaziqSyah <111070025+HaziqSyah@users.noreply.github.com>
2022-08-11 22:44:27 +0800

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+           <!-- Footer Section Starts -->
+           <div class="footer">
+                <div class="wrapper">
+                <p class="text-center">2022 All rights reserved, Cupcake.Developed By <a href="#"> SETP Group 8 </a></p>
+                </div> 
+          </div>
+     </body>
+</html>
